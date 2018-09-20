7a5a6537e564b7a84900f4aa6ad4bea29adfbd45
anqianqi1 <43048534+anqianqi1@users.noreply.github.com>
2018-09-19 21:10:29 -0400

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,5 +1,8 @@
 import subprocess
 import os
+#use the subprocess to exetuate the ffmpeg and convert images into video
 
 subprocess.call(
-	'ffmpeg -framerate 1/5 -i img%03d.jpg -c:v libx264-vf fps=25 -pix_fmt yuv420p out.mp4',shell=True)
+	'ffmpeg -framerate 1/5 -f image2 -s 1920*1080 -i /home/ece-student/EC\ 601/image/img%03d.jpg -crf 25 -pix_fmt yuv420p out.mp4',shell=True)
+# use ffmpeg to detect the images in /home/ece-student/EC\ 601/image/ and images with name 'img00'started, convert images into 
+#1920*1080 and give me a mp4 video
